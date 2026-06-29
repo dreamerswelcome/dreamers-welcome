1649f4ae0b5b3d6afae92292815d33f87bbb3e98
akshita-tomar <122252793+akshita-tomar@users.noreply.github.com>
2026-06-29 12:05:22 +0530

    added whellio logic

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,30 +1,66 @@
-import Document, { DocumentContext } from 'next/document'
+
+import Document, {
+  DocumentContext,
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
 export default class MyDocument extends Document {
-    static async getInitialProps(ctx: DocumentContext) {
-        const sheet = new ServerStyleSheet()
-        const originalRenderPage = ctx.renderPage
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
 
-        try {
-            ctx.renderPage = () =>
-                originalRenderPage({
-                    enhanceApp: (App) => (props) =>
-                        sheet.collectStyles(<App {...props} />),
-                })
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        })
 
-            const initialProps = await Document.getInitialProps(ctx)
-            return {
-                ...initialProps,
-                styles: (
-                    <>
-                        {initialProps.styles}
-                        {sheet.getStyleElement()}
-                    </>
-                ),
-            }
-        } finally {
-            sheet.seal()
-        }
+      const initialProps = await Document.getInitialProps(ctx)
+
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      }
+    } finally {
+      sheet.seal()
     }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+
+        <body>
+          <Main />
+          <NextScript />
+
+          {/* Wheelio Configuration */}
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.wloShopID = "8671e4c5-633a-479f-6ec3-08dec3a87b0e";
+              `,
+            }}
+          />
+
+          {/* Wheelio Script */}
+          <script
+            async
+            src="https://cdn.wheelio-app.com/app/index.min.js"
+          />
+        </body>
+      </Html>
+    )
+  }
 }
